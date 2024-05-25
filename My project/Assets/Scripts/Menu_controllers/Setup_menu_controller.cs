0e16eca39955ca7864774c339c28146471aac31c
Dominik Kłaput <115083686+dkx12341@users.noreply.github.com>
2024-05-25 08:14:05 +0200

    minor changes

--- My project/Assets/Scripts/Menu_controllers/Setup_menu_controller.cs
+++ My project/Assets/Scripts/Menu_controllers/Setup_menu_controller.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Runtime.CompilerServices;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -15,33 +14,28 @@ public class Setup_menu_controller : MonoBehaviour
     // Start is called before the first frame update
     public TMP_Dropdown track_selector;
     public TMP_Dropdown car_selector;
-    public string[] levels_list;
-    public string[] cars_list;
+    public UnityEngine.Object[] levels_list;
+    public UnityEngine.Object[] cars_list;
     private string currently_selected_level;
     public SO_string Selected_car;
     public void Start()
     {
-      
         track_selector.ClearOptions();
         List<string> tracks = new List<string>();
-        levels_list = System.IO.Directory.GetFiles("Assets/Resources/Scenes/Levels", "*.unity", SearchOption.AllDirectories);
-        for(int i =0;  levels_list.Length>i; i++)
+        levels_list = Resources.LoadAll("Scenes/Levels");
+        for (int i =0;  levels_list.Length>i; i++)
         {
-            levels_list[i] = Path.GetFileName(levels_list[i]);
-            levels_list[i] = levels_list[i].Substring(0, levels_list[i].Length - 6);
-            tracks.Add(levels_list[i]);
+            tracks.Add(levels_list[i].name);
         }
         track_selector.AddOptions(tracks);
         currently_selected_level = tracks[0];
 
         car_selector.ClearOptions();
         List<string> cars = new List<string>();
-        cars_list = System.IO.Directory.GetFiles("Assets/Resources/Prefabs/Cars", "*.prefab", SearchOption.AllDirectories);
+        cars_list = Resources.LoadAll("Prefabs/Cars");
         for (int i = 0; cars_list.Length > i; i++)
         {
-            cars_list[i] = Path.GetFileName(cars_list[i]);
-            cars_list[i] = cars_list[i].Substring(0, cars_list[i].Length - 7);
-            cars.Add(cars_list[i]);
+            cars.Add(cars_list[i].name);
         }
         car_selector.AddOptions(cars);
         Selected_car.Str = cars[0];
