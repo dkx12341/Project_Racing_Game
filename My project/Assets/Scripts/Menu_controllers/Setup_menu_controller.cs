0c795550dab19bcbe9ae6fa7d7030f3b038dc088
Dominik Kłaput <115083686+dkx12341@users.noreply.github.com>
2024-06-06 13:36:34 +0200

    Fixed Build!!!!

--- My project/Assets/Scripts/Menu_controllers/Setup_menu_controller.cs
+++ My project/Assets/Scripts/Menu_controllers/Setup_menu_controller.cs
@@ -1,38 +1,48 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
-using UnityEngine.Windows;
 
 public class Setup_menu_controller : MonoBehaviour
 {
     // Start is called before the first frame update
     public TMP_Dropdown track_selector;
     public TMP_Dropdown car_selector;
-    public UnityEngine.Object[] levels_list;
+    //public Scene[] levels_list;
     public UnityEngine.Object[] cars_list;
     private string currently_selected_level;
     public SO_string Selected_car;
     public void Start()
     {
+        
         track_selector.ClearOptions();
         List<string> tracks = new List<string>();
-        levels_list = Resources.LoadAll("Scenes/Levels");
-        for (int i =0;  levels_list.Length>i; i++)
+        int sceneCount = SceneManager.sceneCountInBuildSettings;
+        string[] all_scenes_list = new string[sceneCount];
+
+        for (int i = 0; i < sceneCount; i++)
         {
-            tracks.Add(levels_list[i].name);
+            all_scenes_list[i] = Path.GetFileNameWithoutExtension(SceneUtility.GetScenePathByBuildIndex(i));
+        }
+
+        for (int i = 0; i < all_scenes_list.Length; i++)
+        {
+            string scene_name = all_scenes_list[i];
+            if (scene_name.Substring(0,6) == "track_")
+            {
+                tracks.Add(scene_name.Substring(6));
+
+            }
         }
         track_selector.AddOptions(tracks);
-        currently_selected_level = tracks[0];
+        currently_selected_level = "track_" + tracks[0];
+    
 
         car_selector.ClearOptions();
         List<string> cars = new List<string>();
         cars_list = Resources.LoadAll("Prefabs/Cars");
+
         for (int i = 0; cars_list.Length > i; i++)
         {
             cars.Add(cars_list[i].name);
@@ -42,7 +52,7 @@ public class Setup_menu_controller : MonoBehaviour
     }
     public void on_dropdown_update()
     {
-        currently_selected_level =  track_selector.options[track_selector.value].text;
+        currently_selected_level = "track_" + track_selector.options[track_selector.value].text;
         Selected_car.Str = car_selector.options[car_selector.value].text;
 
     }
